f2b04ad1b36e0cb3a543900fe6989592ce17d7c9
Rahul Gupta <rahulgupta.ra.com@gmail.com>
2023-03-28 10:34:00 +0530

    singleton class of prisma

--- server/index.js
+++ server/index.js
@@ -5,9 +5,6 @@ const cookieParser = require('cookie-parser')
 const swaggerUi = require("swagger-ui-express");
 const YAML = require('yamljs');
 const path = require('path');
-const {PrismaClient} = require('@prisma/client')
-const db = new PrismaClient();
-
 const errorHandler = require('./api/v1/middlewares/error-handler');
 const notFound = require("./api/v1/middlewares/not-found");
 const v1 = require('./api/v1/routes');
@@ -15,45 +12,39 @@ const v1 = require('./api/v1/routes');
 let port = process.env.PORT || 3000;
 let app = express();
 
-// * cors
-let corsOptions = {
-  origin: '*',
-}
+// cors
+let corsOptions = {origin: '*',}
 app.use(cors(corsOptions));
 
 
-// * express parse
+// express parse
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
 
-// * swagger-jsdoc
+// swagger docs
 let swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yml'));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
-// * // check server health
+// main api
 app.get('/health', (req, res) => {
-  res.status(200).json({
-    message: "Server is up and running"
-  });
+  res.status(200).json({status: "Running"});
 });
 
-// * api v1  server
 app.use("/v1/", v1);
 
 app.use(errorHandler);
 app.use(notFound);
 
 
-(async function () {
-  try {
-    app.listen(port, () => {
-      console.log(`app listening to http://localhost:${port}`);
-    });
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-})();
+// star the server
+try {
+  app.listen(4004, () => {
+    console.log(`app listening to http://localhost:${port}`);
+  });
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
